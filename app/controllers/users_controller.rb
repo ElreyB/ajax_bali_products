23c7e9ae6fd67733355a34af17e9a938c9cae97b
Elrey Belmonti <elbelmonti@gmail.com>
2017-12-10 01:49:30 -0800

    Add password validations to ensure a user's password is sufficiently complex.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       flash[:notice] = "Welcome to Ajax Bali Products"
       session[:user_id] = user.id
       redirect_to '/'
     else
       flash[:notice] = "Something went wrong:"
-      redirect_to '/sign_up'
+      render :new
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password validations: false
   has_many :orders
+
+  validates_confirmation_of :password
+  validates :password, :presence => true, :length => { minimum: 6 }, format: { with: /\A(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/,
+    message: "must contain at least 6 characters, a number, a lowercase letter, uppercase letter, a symbol."  }
+  validates :email, :presence => true, :uniqueness => true, format: { with: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, multiline: true}
+
 
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
