8d7d7e15319d891622353d67c1d70c77d8f38ea7
Elrey Belmonti <elbelmonti@gmail.com>
2017-12-09 23:14:26 -0800

    Add product update and delete functionality for admins and  flash messages for adding, updating, and destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,6 +28,28 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product =Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Your product has been update!"
+      redirect_to product_path(@product)
+    else
+      flash[:alert] = "Please fix these errors:"
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "Product has been deleted."
+    redirect_to products_path
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price, :image, :in_stock)
