45ffb49874aaa1b6d35324540dfb9a3a9db5a41d
Elrey Belmonti <elbelmonti@gmail.com>
2017-12-10 08:52:52 -0800

    add quanity validation for order items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,13 +3,17 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @item = @order.order_items.new(item_params)
-    @order.save
-    session[:order_id] = @order.id
 
-    flash[:notice] = "Item added to cart!"
-      respond_to do |format|
-        format.html { redirect_to products_path }
-        format.js
+    if @order.save
+      session[:order_id] = @order.id
+
+      flash[:notice] = "Item added to cart!"
+        respond_to do |format|
+          format.html { redirect_to products_path }
+          format.js
+        end
+      else
+        flash[:alert] = "Looks like something went wrong:"
       end
     redirect_to products_path
   end
