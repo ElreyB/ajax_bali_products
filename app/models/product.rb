eb1436ea871dddbb822c6b3558d103da1cd38e90
Elrey Belmonti <elbelmonti@gmail.com>
2017-12-10 01:01:11 -0800

    add spec and rails helper files

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ApplicationRecord
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100#" }, default_url: ":style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :name, :description, :price, presence: true
 end
