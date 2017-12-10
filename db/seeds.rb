573ff4b78ed89880b09646152bd297dd08d82f41
Elrey Belmonti <elbelmonti@gmail.com>
2017-12-09 22:33:17 -0800

    refactor seed file to add in_stock amount and image for product

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,13 @@ class Seed
   end
 
   def generate_products
+    Product.destroy_all
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop, in_stock: rand(10..100), image: File.new("app/assets/images/image#{i}.jpg"))
     end
   end
 end
 
 Seed.begin
+
+p "Created #{Product.count} products"
